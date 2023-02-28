711308d235717e5cc1cb57432b9fbf6e7763d920
MasonPaprzycki <93019015+MasonPaprzycki@users.noreply.github.com>
2023-02-28 08:33:35 -0700

    maven stuff

---
+++ MavenProject/gpt-yes/src/main/java/gptyes/StringToRandom.java
@@ -0,0 +1,13 @@
+package gptyes;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringToRandom
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
