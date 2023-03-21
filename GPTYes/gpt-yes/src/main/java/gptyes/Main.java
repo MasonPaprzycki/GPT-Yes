a497c6645ac8fded4ad08578dd2f74c1f3dfaace
MasonPaprzycki <93019015+MasonPaprzycki@users.noreply.github.com>
2023-03-21 12:47:00 -0600

    htkjhjkg

--- GPTYes/gpt-yes/src/main/java/gptyes/Main.java
+++ GPTYes/gpt-yes/src/main/java/gptyes/Main.java
@@ -2,40 +2,16 @@ package gptyes;
 
 import java.util.Scanner;
 
-
-
-
-
-
-/**
- * Hello world!
- *
- */
 public class Main
 {
-    public static void main( String[] args )
-
-    {
-
+    public static void main( String[] args ){
         Scanner input = new Scanner(System.in);
         System.out.println("enter in a sentence to randomize:");
-
         String sentence = input.nextLine();
-
         System.out.println("enter in a true random number randomly tap on your number pad!");
-        
-        
         int trueRandomNumber =Integer.parseInt(input.nextLine());
-
         input.close();
-
-        
-        
-  
-     
         String print = RandomString.getRandomString(sentence, trueRandomNumber);
-
         System.out.println(print);
-        
     }
 }
--- GPTYes/gpt-yes/src/main/java/gptyes/RandomString.java
+++ GPTYes/gpt-yes/src/main/java/gptyes/RandomString.java
@@ -1,68 +1,42 @@
 package gptyes;
 
-
 import com.groupdocs.search.Index;
-
+// this api is not mine 
+// I retrieved this api from 
 
 public class RandomString{
 
-    public static final String no = "h5n1";
-   
-  
-
-
- 
-    
-
+   // this method manages complexity because it makes the flow of the program 
+   // more simple and i have less steps i have to think through 
    
     static String getRandomSynonym(String word, int trueRandomNumber){
         Index pass = new Index();
-       
         String[] synonyms = pass.getDictionaries().getSynonymDictionary().getSynonyms(word);
         pass.close();
         int length = synonyms.length;
-
-        
         if(length>1){
             return synonyms[(trueRandomNumber %(length))-1] ;
-
         }
         else if(length==1){
             return synonyms[0];
-
         }
         else{
             return word;
         }
-        
     }
 
     static String getRandomString(String text, int trueRandomNumber){
-
-
+        // this list is meaningful because I am taking input from the user 
+        //and i have to seperate each of the words and change them independently 
         String[] sillyString = text.split(" ");
+
         for(int i =0; i<sillyString.length; i++){
-
-
-
             if(!sillyString[i].equals("a")){
                 sillyString[i] = getRandomSynonym(sillyString[i], trueRandomNumber);
-              
             }
-          
-            
-          
             System.out.println(sillyString[i]);
         }
-     
         String end = String.join(" ", sillyString); 
-        
-
         return end;
     }
-    
-  
-      
-
-  
 }
