b29defc35e50747723f9bfc22649ed651b18ece8
MasonPaprzycki <93019015+MasonPaprzycki@users.noreply.github.com>
2023-03-21 11:42:50 -0600

    reorganized

--- GPTYes/gpt-yes/src/main/java/gptyes/RandomString.java
+++ GPTYes/gpt-yes/src/main/java/gptyes/RandomString.java
@@ -1,12 +1,6 @@
 package gptyes;
 
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import com.groupdocs.search.Index;
 
 
@@ -18,34 +12,12 @@ public class RandomString{
  
     
 
-    static String[] getSynonyms(String word){
-        ArrayList<String> listSynonyms = new ArrayList<String>();
-
    
-
+    static String getRandomSynonym(String word, int trueRandomNumber){
         Index pass = new Index();
-
+       
         String[] synonyms = pass.getDictionaries().getSynonymDictionary().getSynonyms(word);
         pass.close();
-       
-        
-
-
-        for (String synonym : synonyms) {
-            listSynonyms.add(synonym);
-        
-
-     
-        }
-        String[] stringArray = new String[listSynonyms.size()];
-        stringArray = listSynonyms.toArray(stringArray);
-     
-        return stringArray;
-
-    }
-
-    static String getRandomSynonym(String word, int trueRandomNumber){
-        String[] synonyms = getSynonyms(word);
         int length = synonyms.length;
 
         
@@ -59,7 +31,6 @@ public class RandomString{
         }
         else{
             return word;
-
         }
         
     }
@@ -69,12 +40,12 @@ public class RandomString{
 
         String[] sillyString = text.split(" ");
         for(int i =0; i<sillyString.length; i++){
-            String var = getRandomSynonym(sillyString[i], trueRandomNumber);
 
 
-            // for some reason it prints h5n1 even with this code thats really really weird 
-            if(var!="h5n1"){
-                sillyString[i] = var;
+            // turning a into "h5n1" for some reason
+
+            if(sillyString[i]!="a"){
+                sillyString[i] = getRandomSynonym(sillyString[i], trueRandomNumber);
             }
           
             System.out.println(sillyString[i]);
